897fdf005535bcd70b5aeb8d0629026ecfff8060
jeyashrii <jeyashri30@gmail.com>
2025-08-02 20:24:29 +0530

    initial-commmit

---
+++ src/FindMaxin2DArray.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+public class FindMaxin2DArray {
+    public static void main(String[] args) {
+        int[][] arr = {{2, 3, 5, 6}, {8, 9, 47}};
+        System.out.println(Arrays.toString(findMaxPos(arr)));
+        int[][] arr1 = {{2, 3, 5, 6}, {8, 9, 47}};
+        System.out.println(findMaxNum(arr1));
+    }
+//return the position of maximum value
+
+    static int[] findMaxPos(int[][] arr) {
+        int max = arr[0][0];
+        int[] position = {0, 0};
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                if (arr[row][col] > max) {
+                    max = arr[row][col];
+                    position[0] = row;
+                    position[1] = col;
+                }
+            }
+        }
+        return position;
+    }
+
+    //return the maximum value
+    static int findMaxNum(int[][] arr) {
+        int max = Integer.MIN_VALUE;
+        for (int[] rows:arr) {
+            for (int element:rows) {
+                if (element> max) {
+                    max = element;
+                }
+            }
+        }
+        return max;
+    }
+}
---
+++ src/FindMin.java
@@ -0,0 +1,19 @@
+public class FindMin {
+    public static void main(String[]args){
+        int[] arr={1,4,5,-7,9,0};
+        System.out.println(min(arr));
+    }
+    //assume arr.length!=0
+    //return the minimum value in the array
+        static int min(int[] arr){
+        int minimum=arr[0];
+            for(int i=0;i<arr.length;i++){
+                if(arr[i]<minimum){
+                    minimum=arr[i];
+                }
+            }
+            return minimum;
+        }
+
+
+}
